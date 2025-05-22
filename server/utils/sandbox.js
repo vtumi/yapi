85fd67b3a6b525c7b105d68ed8ead9628f8030d9
Tumi <mphphy@gmail.com>
2025-05-22 17:18:09 +0800

    Update sandbox.js

--- server/utils/sandbox.js
+++ server/utils/sandbox.js
@@ -4,11 +4,15 @@ module.exports = async function sandboxFn(context, script) {
     // 创建 safeify 实例
     const safeVm = new Safeify({
         timeout: 3000,
-        asyncTimeout: 60000
+        asyncTimeout: 10000,
+        unrestricted: true,
+        quantity: 4,
+        memoryQuota: 500,
+        cpuQuota: 0.5
     })
 
-    script += "; return this;";
-    // 执行动态代码
+    // 执行动态代码·
+    script += "\n  return {mockJson, resHeader, httpCode, delay, Random}";
     const result = await safeVm.run(script, context)
 
     // 释放资源
